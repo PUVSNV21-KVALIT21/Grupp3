2716dc42312fd7961d974c1f83c7a4da1798cb40
Jesperere <75724548+Jesperere@users.noreply.github.com>
2022-04-27 14:33:41 +0200

    Add files via upload

---
+++ Hakims Livs/Hakims Livs/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hakims_Livs.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
