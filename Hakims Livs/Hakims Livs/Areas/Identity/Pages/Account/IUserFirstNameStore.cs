522e3549939a41b40ce1a58eaa4dbe34b9212225
ErikStenberg1 <erik.stenberg02@gmail.com>
2022-05-03 10:03:50 +0200

    Register fields added

---
+++ Hakims Livs/Hakims Livs/Areas/Identity/Pages/Account/IUserFirstNameStore.cs
@@ -0,0 +1,8 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace Hakims_Livs.Areas.Identity.Pages.Account
+{
+    internal interface IUserFirstNameStore<T>
+    {
+    }
+}
---
+++ Hakims Livs/Hakims Livs/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hakims_Livs.Models
+{
+    public class Customer : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public DateTime Created { get; set; }
+        public int? Age { get; set; }
+        [NotMapped]
+        public string FullName { get { return this.FirstName + " " + this.LastName; } }
+    }
+}
