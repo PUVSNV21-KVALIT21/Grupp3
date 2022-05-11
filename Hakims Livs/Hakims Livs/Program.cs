9245020a592c61c6ae7e3b703e24237135be9f5e
Elin-Sjo <75704122+Elin-Sjo@users.noreply.github.com>
2022-05-11 15:40:40 +0200

    admin page, add role to admin

--- Hakims Livs/Hakims Livs/Program.cs
+++ Hakims Livs/Hakims Livs/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<Customer>(options => options.SignIn.RequireConfirmedAccount = true)
+     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddRazorPages();
